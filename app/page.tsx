1c83874100a5413d6e18e05c778256987ffe4ebc
Alok Kumar <helloalokkumar108@gmail.com>
2026-02-03 02:25:47 +0530

    fix oauth for production

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import { Suspense } from "react";
 import TopicList from "@/components/topic/topic-list";
 import TopicCreateForm from "@/components/topic/topic-create-form";
